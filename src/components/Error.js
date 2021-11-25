795ab6f165524c625c275e144cc5c48463b0f164
Rajan kumar <krcpr007@gmail.com>
2021-11-25 16:44:57 +0530

    Error done

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 export default class Error extends Component {
     render() {
         return (
-            <div style={{height:"500px"}} className="container">
+            <div  className="container">
                 <div className="Error-page">
-                <h1>Error</h1>
+                <h1>Error!!</h1>
                 <h1>404</h1> Page does not exist...
                 <Link style={{textDecoration:"none", color:"#64ffda"}} to="/">Go to Home</Link>
                 </div>
