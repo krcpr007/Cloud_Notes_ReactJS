501cf7dda0b78cf2698a25209ecd73e0054e80c8
Rajan kumar <krcpr007@gmail.com>
2021-12-18 15:48:29 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import { Navbar } from './components/Navbar';
-import { Home } from './components/Home';
-import { About } from './components/About';
-import { Users } from './components/Users';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import NoteState from './context/NoteState';
-import { Footer } from './components/Footer';
-import { Signup } from './components/Signup';
-import { Login } from './components/Login';
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import NoteState from "./context/NoteState";
+import { Footer } from "./components/Footer";
+import { Signup } from "./components/Signup";
+import { Login } from "./components/Login";
+import Alert from "./components/Alert";
+import Error from "./components/Error";
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   return (
-    <> 
-    <NoteState>
-      <Router>
-        <Navbar />
-        {/* <Home /> */}
-        <Switch>
-          <Route exact  path="/about">
-            <About />
-          </Route>
-          <Route exact path="/users">
-            <Users />
-          </Route>
-          <Route exact path="/home">
-            <Home />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-          {/* <Route exact path="/login">
-            <Login />
-          </Route> */}
-        </Switch>
-          <Footer/>
-      </Router>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert} />
+          <Switch>
+            <Route exact path="/">
+              <Home showAlert={showAlert} />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/login">
+              <Login showAlert={showAlert} />
+            </Route>
+            <Route exact path="/signup">
+              <Signup showAlert={showAlert} />
+            </Route>
+            <Route showAlert={showAlert} component={Error} />
+          </Switch>
+          <Footer />
+        </Router>
       </NoteState>
     </>
   );
